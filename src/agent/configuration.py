bcc53261599b3fdb6ff2e084b53d02fe32cf0745
vivekpatel99 <vivpatel99@gmail.com>
2025-07-14 11:21:14 +0000

    first working simpel agent with wikipedia tool

---
+++ src/agent/configuration.py
@@ -0,0 +1,11 @@
+from typing import TypedDict
+
+
+class Configuration(TypedDict):
+    """Configurable parameters for the agent.
+
+    Set these when creating assistants OR when invoking the graph.
+    See: https://langchain-ai.github.io/langgraph/cloud/how-tos/configuration_cloud/
+    """
+
+    my_configurable_param: str
--- src/agent/graph.py
+++ src/agent/graph.py
@@ -3,37 +3,12 @@
 Returns a predefined response. Replace logic and configuration as needed.
 """
 
-from __future__ import annotations
-
-from dataclasses import dataclass
-from typing import Any, Dict, TypedDict
-
 from langchain_core.runnables import RunnableConfig
-from langgraph.graph import StateGraph
+
+from agent.states import PlanningState
 
 
-class Configuration(TypedDict):
-    """Configurable parameters for the agent.
-
-    Set these when creating assistants OR when invoking the graph.
-    See: https://langchain-ai.github.io/langgraph/cloud/how-tos/configuration_cloud/
-    """
-
-    my_configurable_param: str
-
-
-@dataclass
-class State:
-    """Input state for the agent.
-
-    Defines the initial structure of incoming data.
-    See: https://langchain-ai.github.io/langgraph/concepts/low_level/#state
-    """
-
-    changeme: str = "example"
-
-
-async def call_model(state: State, config: RunnableConfig) -> Dict[str, Any]:
+async def call_model(state: PlanningState, config: RunnableConfig) -> dict[str, Any]:
     """Process input and returns output.
 
     Can use runtime configuration to alter behavior.
@@ -41,14 +16,14 @@ async def call_model(state: State, config: RunnableConfig) -> Dict[str, Any]:
     configuration = config["configurable"]
     return {
         "changeme": "output from call_model. "
-        f'Configured with {configuration.get("my_configurable_param")}'
+        f"Configured with {configuration.get('my_configurable_param')}"
     }
 
 
-# Define the graph
-graph = (
-    StateGraph(State, config_schema=Configuration)
-    .add_node(call_model)
-    .add_edge("__start__", "call_model")
-    .compile(name="New Graph")
-)
+# # Define the graph
+# graph = (
+#     StateGraph(State, config_schema=Configuration)
+#     .add_node(call_model)
+#     .add_edge("__start__", "call_model")
+#     .compile(name="New Graph")
+# )
