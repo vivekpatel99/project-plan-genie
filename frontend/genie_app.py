cdd489680ff227d60c31b19e12a21367452a7211
vivekpatel99 <vivpatel99@gmail.com>
2025-08-05 18:31:58 +0200

    more or less working, same text in shadow issue

--- frontend/genie_app.py
+++ frontend/genie_app.py
@@ -51,70 +51,70 @@ async def stream_graph_responses(
     graph: CompiledStateGraph,
     config: dict,
 ) -> AsyncGenerator[str]:
-    async for message_chunk in graph.astream(
-        input=user_input,
-        config=config,
-        stream_mode="updates",  # ["updates", "messages"],
-    ):
-        # if stream_mode == "messages": # TODO: grab graph name here and use token streaming from messages
-        #     ...
-        # if stream_mode == "updates" and isinstance(message_chunk, dict):
-        with st.status(label="Thinking...", expanded=True) as status:
-            keys = list(message_chunk.keys())
-            graph_name = keys[0]
-            status.write(f"📍 Current Stage: **{graph_name}**")
-            chunk = message_chunk[graph_name]
+    with st.status(label="Thinking...", expanded=True) as status:
+        async for stream_mode, message_chunk in graph.astream(
+            input=user_input,
+            config=config,
+            stream_mode=["updates", "messages"],
+        ):
+            # if stream_mode == "messages": # TODO: grab graph name here and use token streaming from messages
+            #     ...
+            if stream_mode == "updates" and isinstance(message_chunk, dict):
+                keys = list(message_chunk.keys())
+                graph_name = keys[0]
+                status.write(f"📍 Current Stage: **{graph_name}**")
+                chunk = message_chunk[graph_name]
 
-            print(chunk)
-            if isinstance(chunk, tuple) and isinstance(chunk[0], Interrupt):
-                interrupt_message = chunk[0].value
-                message = interrupt_message["message"]
-                tools_calls = interrupt_message["tool_call"]["name"]
-                yield f"\n\n{message} => **{tools_calls}**"
+                print(chunk)
+                if isinstance(chunk, tuple) and isinstance(chunk[0], Interrupt):
+                    interrupt_message = chunk[0].value
+                    message = interrupt_message["message"]
+                    tools_calls = interrupt_message["tool_call"]["name"]
+                    yield f"\n\n{message} => **{tools_calls}**"
 
-            if isinstance(chunk, dict) and "messages" in chunk:  # messages
-                last_message = chunk["messages"][-1]
+                if isinstance(chunk, dict) and "messages" in chunk:  # messages
+                    last_message = chunk["messages"][-1]
 
-                if isinstance(last_message, AIMessage) and last_message.tool_calls:
-                    tool_call = last_message.tool_calls[-1]
-                    yield f"Tool call: **{tool_call['name']}**"
-                    # continue
+                    if isinstance(last_message, AIMessage) and last_message.tool_calls:
+                        tool_call = last_message.tool_calls[-1]
+                        yield f"Tool call: **{tool_call['name']}**"
+                        # continue
 
-                parts = re.split(THINK_REGEX, last_message.content, flags=re.DOTALL)
+                    parts = re.split(THINK_REGEX, last_message.content, flags=re.DOTALL)
 
-                # The parts will alternate between reply text and thought text.
-                # Reply parts are at even indices, thoughts at odd indices.
-                reply = "".join(parts[::2]).strip()
-                thoughts = parts[1::2]
-                if thoughts:
-                    status.markdown(f"🤔 {thoughts[0]}")
-                    status.update(label="Complete!", state="complete", expanded=False)
+                    # The parts will alternate between reply text and thought text.
+                    # Reply parts are at even indices, thoughts at odd indices.
+                    reply = "".join(parts[::2]).strip()
+                    thoughts = parts[1::2]
+                    if thoughts:
+                        status.markdown(f"🤔 {thoughts[0]}")
+                    yield reply
 
-                yield reply
+        # Update status display
+        status.update(label="Complete!", state="complete", expanded=False)
 
 
 if prompt := st.chat_input("Please Write Detail Project Description"):
     # first add the message to message_history
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.session_state.messages.append({"role": "human", "content": prompt})
 
-    with st.chat_message("user"):
+    with st.chat_message("human"):
         st.markdown(prompt)
 
     user_input_message = {"messages": [HumanMessage(content=prompt)]}
 
     # thread_state = clarify_subgraph.get_state(config=CONFIG)
-    with st.chat_message("assistant", avatar="🧞‍♀️"):
+    with st.chat_message("🧞‍♀️"):
         ai_message = st.write_stream(
             stream_graph_responses(
                 user_input=user_input_message,
                 graph=test_graph,
                 config=CONFIG,
-                # status=None,
             ),
         )
-        # Update status display
 
-        print("#############")
+    print("#############")
     print("ai_message", ai_message)
+
     # the message to message_history
-    st.session_state.messages.append({"role": "assistant", "content": ai_message})
+    st.session_state.messages.append({"role": "🧞‍♀️", "content": ai_message})
