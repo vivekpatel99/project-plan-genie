3b46feddd7a3727a860de7a35b634328131188e0
vivekpatel99 <vivpatel99@gmail.com>
2025-08-05 18:00:49 +0200

    first okesh working ui

--- frontend/genie_app.py
+++ frontend/genie_app.py
@@ -1,47 +1,120 @@
+import re
+import sys
+from collections.abc import AsyncGenerator
+from pathlib import Path
+
 import rootutils
 import streamlit as st
-from langgraph.checkpoint.memory import InMemorySaver
+from langchain_core.messages import AIMessage, BaseMessage, HumanMessage
+from langgraph.graph.state import CompiledStateGraph
+from langgraph.types import Interrupt
+from loguru import logger
 
 rootutils.setup_root(__file__, indicator=".git", pythonpath=True)
-from src.agent.clarification_agent_subgraph import clarify_builder  # noqa: E402
+from utils import test_graph  # noqa: E402
+
+# from src.agent.clarification_agent_subgraph import clarify_builder
+config = {
+    "handlers": [
+        {"sink": sys.stdout, "level": "ERROR", "colorize": True},
+        {
+            "sink": f"{Path.cwd().parent.parent / f'{__name__}.log'}",
+            "enqueue": True,
+            "level": "DEBUG",
+            "rotation": "10 MB",
+            "compression": "zip",
+        },
+    ],
+}
+
+
+logger.configure(**config)
 
 CONFIG = {"configurable": {"thread_id": "1"}}
+THINK_REGEX = r"<think>(.*?)</think>"
 
-st.title("Project Planning Genie")
+st.title("Project Planning Genie 🧞‍♀️")
 
-if "message_history" not in st.session_state:
-    st.session_state["message_history"] = []
-
+# Create an empty list to store messages
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
 
 # loading the conversation history
-for message in st.session_state["message_history"]:
+for message in st.session_state["messages"]:
     with st.chat_message(message["role"]):
-        st.text(message["content"])
-
-clarify_subgraph = clarify_builder.compile(name="Clarify with User", checkpointer=InMemorySaver())
-user_input = st.chat_input("Please Write Detail Project Description")
+        st.markdown(message["content"])
 
 
-# async def chat_astream(user_input: dict[str, BaseMessage], config: dict):
-#     async for message_chunk, metadata in clarify_subgraph.astream(
-#         input=user_input,
-#         config=config,
-#         stream_mode="messages",
-#     ):
-#         clarification_object = ClarifyWithUser.model_validate(message_chunk.content)
-#         yield clarification_object.question
+async def stream_graph_responses(
+    *,
+    user_input: dict[str, BaseMessage],
+    graph: CompiledStateGraph,
+    config: dict,
+) -> AsyncGenerator[str]:
+    async for message_chunk in graph.astream(
+        input=user_input,
+        config=config,
+        stream_mode="updates",  # ["updates", "messages"],
+    ):
+        # if stream_mode == "messages": # TODO: grab graph name here and use token streaming from messages
+        #     ...
+        # if stream_mode == "updates" and isinstance(message_chunk, dict):
+        with st.status(label="Thinking...", expanded=True) as status:
+            keys = list(message_chunk.keys())
+            graph_name = keys[0]
+            status.write(f"📍 Current Stage: **{graph_name}**")
+            chunk = message_chunk[graph_name]
+
+            print(chunk)
+            if isinstance(chunk, tuple) and isinstance(chunk[0], Interrupt):
+                interrupt_message = chunk[0].value
+                message = interrupt_message["message"]
+                tools_calls = interrupt_message["tool_call"]["name"]
+                yield f"\n\n{message} => **{tools_calls}**"
+
+            if isinstance(chunk, dict) and "messages" in chunk:  # messages
+                last_message = chunk["messages"][-1]
+
+                if isinstance(last_message, AIMessage) and last_message.tool_calls:
+                    tool_call = last_message.tool_calls[-1]
+                    yield f"Tool call: **{tool_call['name']}**"
+                    # continue
+
+                parts = re.split(THINK_REGEX, last_message.content, flags=re.DOTALL)
+
+                # The parts will alternate between reply text and thought text.
+                # Reply parts are at even indices, thoughts at odd indices.
+                reply = "".join(parts[::2]).strip()
+                thoughts = parts[1::2]
+                if thoughts:
+                    status.markdown(f"🤔 {thoughts[0]}")
+                    status.update(label="Complete!", state="complete", expanded=False)
+
+                yield reply
 
 
-# if user_input:
-#     # first add the message to message_history
-#     st.session_state["message_history"].append({"role": "user", "content": user_input})
-#     with st.chat_message("user"):
-#         st.text(user_input)
-#     user_input_message = {"messages": [HumanMessage(content=user_input)]}
-#     ai_message = st.write_stream(chat_astream(user_input_message, CONFIG))
-#     thread_state = clarify_subgraph.get_state(config=CONFIG)
+if prompt := st.chat_input("Please Write Detail Project Description"):
+    # first add the message to message_history
+    st.session_state.messages.append({"role": "user", "content": prompt})
 
-#     # # first add the message to message_history
-#     st.session_state["message_history"].append({"role": "assistant", "content": ai_message})
-#     with st.chat_message("assistant"):
-#         st.text(ai_message)
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    user_input_message = {"messages": [HumanMessage(content=prompt)]}
+
+    # thread_state = clarify_subgraph.get_state(config=CONFIG)
+    with st.chat_message("assistant", avatar="🧞‍♀️"):
+        ai_message = st.write_stream(
+            stream_graph_responses(
+                user_input=user_input_message,
+                graph=test_graph,
+                config=CONFIG,
+                # status=None,
+            ),
+        )
+        # Update status display
+
+        print("#############")
+    print("ai_message", ai_message)
+    # the message to message_history
+    st.session_state.messages.append({"role": "assistant", "content": ai_message})
--- frontend/utils.py
+++ frontend/utils.py
@@ -1,44 +1,144 @@
-from collections.abc import AsyncGenerator
-from typing import Any
+import asyncio
+from typing import Literal
 
-from langchain_core.messages import AIMessageChunk, ToolCallChunk
-from langgraph.graph.state import CompiledStateGraph
-from langgraph.types import Command
+import rootutils
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from langchain_ollama import ChatOllama
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import END, START, MessagesState, StateGraph
+from langgraph.prebuilt import ToolNode
+from langgraph.types import Command, interrupt
+from loguru import logger
 
-
-async def process_tool_call_chunk(chunk: ToolCallChunk):
-    tool_call_str = ""
-    tool_name = chunk.tool_name
-    args = chunk.get("args", "")
-
-    if tool_name:
-        tool_call_str += f"Tool name: {tool_name}\n"
-    if args:
-        tool_call_str += f"Args: {args}\n"
-
-    return tool_call_str
+rootutils.setup_root(__file__, indicator=".git", pythonpath=True)
+from src.agent.my_mcps import mcp_config  # noqa: E402
 
 
-async def stream_graph_responses(
-    user_input: dict[str, Any] | Command,
-    graph: CompiledStateGraph,
-    **kwargs,
-) -> AsyncGenerator[str]:
-    async for msg_chunk, _ in graph.astream(input=user_input, stream_mode="messages", **kwargs):
-        if isinstance(msg_chunk, AIMessageChunk):
-            finish_reason = msg_chunk.response_metadata.get("finish_reason", "")
-            if finish_reason == "tool_call":
-                yield "\n\n"
-        if msg_chunk.tool_call_chunks:
-            tool_chunk = msg_chunk.tool_call_chunks[0]
-            tool_call_str = await process_tool_call_chunk(tool_chunk)
-            yield tool_call_str
+class States(MessagesState):
+    """State of conversation between Agent and User."""
 
-        else:
-            content = msg_chunk.content
-            if isinstance(content, str):
-                yield content
-            elif isinstance(content, list):
-                yield "\n".join(content)
-            else:
-                yield str(content)
+    # messages: Annotated[list[BaseMessage], add_messages] = []
+
+
+protected_tools: list[str] = ["create_directory", "edit_file", "write_file"]
+# async def display_with_subgraph_status_TODO(user_input: dict[str, BaseMessage], config: dict):
+#     with st.status(label="Thinking...", expanded=True) as status:
+#         async for stream_mode, chunk in test_graph.astream(
+#             input=user_input,
+#             config=config,
+#             stream_mode=["updates", "messages"],
+#         ):
+#             # get info from update mode
+#             if stream_mode == "updates" and isinstance(chunk, dict):
+#                 # chunk is dict of graph name and update
+#                 keys = list(chunk.keys())
+#                 graph_name = keys[0]
+#                 status.write(f"📍 Current Stage: **{graph_name}**")
+
+#             # get info from message mode
+#             if stream_mode == "messages":
+#                 # chunk is tuple of (message, metadata)
+#                 message, metadata = chunk
+
+#                 message = message.content
+
+#                 # parts = re.split(THINK_REGEX, message, flags=re.DOTALL)
+
+#                 # # The parts will alternate between reply text and thought text.
+#                 # # Reply parts are at even indices, thoughts at odd indices.
+#                 # reply = "".join(parts[::2]).strip()
+#                 # thoughts = parts[1::2]
+
+#                 if message == "<think>":
+#                     is_thinking = True
+#                     continue
+#                 if message == "</think>":
+#                     is_thinking = False
+#                 if not is_thinking:
+#                     status.write_stream(f"🤔 {message}")
+#                 else:
+#                     status.update(label="Done!", state="complete", expanded=False)
+#                     yield message
+
+#                 # Update status displays
+
+
+async def test_graph_builder() -> StateGraph:
+    client = MultiServerMCPClient(connections=mcp_config["mcpServers"])
+
+    tools = await client.get_tools()
+
+    llm = ChatOllama(model="qwen3:14b", temperature=0).bind_tools(tools)
+
+    def human_tool_review_node(
+        state: States,
+    ) -> Command[Literal["tools", "assistant_node"]]:
+        """Node is a placeholder for the human to review the final report generation process to verify proper tool call checks before tools are called by the agent."""
+        print("[INFO] human_tool_review_node called")
+        last_message = state["messages"][-1]
+
+        # Ensure we have a valid AI message with tool calls
+        if not isinstance(last_message, AIMessage) or not last_message.tool_calls:
+            msg = "human_tool_review_node called without valid tool calls"
+            logger.error(msg)
+            raise ValueError(msg)
+
+        tool_call = last_message.tool_calls[-1]
+
+        # Stop graph execution and wait for human input
+        human_review: dict = interrupt(
+            {"message": "Your input is required for the following tool:", "tool_call": tool_call},
+        )
+        review_action = human_review.get("action")
+        review_data = human_review.get("data")
+
+        if review_action == "accept":
+            return Command(
+                goto="tools",
+            )
+        return Command(
+            goto="assistant_node",
+            update={
+                "messages": [
+                    HumanMessage(content=review_data),
+                ],
+            },
+        )
+
+    def assistant_node(state: States) -> States:
+        print("[INFO] assistant_node called")
+        response = llm.invoke(
+            [
+                SystemMessage(
+                    content="You are a helpful assistant. You have access to the local filesystem but only within an approved directory. The approved directory is /projects/workspace and all paths must begin with /projects/workspace/. You must use /project/workspace/generated_example directory. if directory does not exists then create it and then give a good name of the <file_name>.md file (for example sw_design.md) and save the generated report in /project/workspace/generated_example directory.",
+                ),
+                *state["messages"],
+            ],
+        )
+        state["messages"] = [*state["messages"], response]
+        return state
+
+    def router(state: States) -> str:
+        print("[INFO] router called")
+        last_message = state["messages"][-1]
+        if isinstance(last_message, AIMessage) and last_message.tool_calls:
+            if any(tool_call["name"] in protected_tools for tool_call in last_message.tool_calls):
+                return "human_tool_review_node"
+            return "tools"
+        return END
+
+    builder = StateGraph(States)
+
+    builder.add_node("assistant_node", assistant_node)
+    builder.add_node("human_tool_review_node", human_tool_review_node)
+    builder.add_node("tools", ToolNode(tools))
+
+    builder.add_edge(START, "assistant_node")
+    builder.add_conditional_edges("assistant_node", router, ["tools", "human_tool_review_node", END])
+    builder.add_edge("tools", "assistant_node")
+
+    return builder.compile(checkpointer=MemorySaver())
+
+
+test_graph = asyncio.run(test_graph_builder())
