c45018f2696162bd55c1048cfe1b5be401c79ea0
vivekpatel99 <vivpatel99@gmail.com>
2025-08-06 11:13:40 +0200

    tool calling and chat

--- frontend/local_genie.py
+++ frontend/local_genie.py
@@ -1,12 +1,14 @@
+"""reference: https://github.com/kenneth-liao/crm-agent/blob/main/frontend/chat_local.py."""
+
 import sys
 from collections.abc import AsyncGenerator
 from pathlib import Path
 from typing import Any
 
 import rootutils
-from langchain_core.messages import AIMessage, HumanMessage
+from langchain_core.messages import AIMessageChunk, HumanMessage
 from langgraph.graph.state import CompiledStateGraph
-from langgraph.types import Command, Interrupt
+from langgraph.types import Command
 from loguru import logger
 
 rootutils.setup_root(__file__, indicator=".git", pythonpath=True)
@@ -29,32 +31,54 @@ async def stream_graph_responses(
         config=config,
         stream_mode=["updates", "messages"],
     ):
-        # if stream_mode == "messages": # TODO: grab graph name here and use token streaming from messages
-        #     ...
-        if stream_mode == "updates" and isinstance(message_chunk, dict):
-            keys = list(message_chunk.keys())
-            graph_name = keys[0]
-            chunk = message_chunk[graph_name]
+        if stream_mode == "messages":  # TODO: grab graph name here and use token streaming from messages
+            message, metadata = message_chunk
+            # node: str | Any = metadata["langgraph_node"]
+            if isinstance(message, AIMessageChunk):
+                if message.response_metadata:
+                    finish_reason = message.response_metadata.get("finish_reason", "")
+                    if finish_reason == "tool_calls":
+                        yield "\n\n"
 
-            if isinstance(chunk, tuple) and isinstance(chunk[0], Interrupt):
-                interrupt_message = chunk[0].value
-                message = interrupt_message["message"]
-                tools_calls = interrupt_message["tool_call"]["name"]
-                yield f"\n\n {message} => **{tools_calls}**"
-                # yield "\n\n"
+                if message.tool_call_chunks:
+                    tool_chunk = message.tool_call_chunks[0]
 
-            if isinstance(chunk, dict) and "messages" in chunk:  # messages
-                last_message = chunk["messages"][-1]
+                    tool_name = tool_chunk.get("name", "")
+                    args = tool_chunk.get("args", "")
 
-                if isinstance(last_message, AIMessage) and last_message.tool_calls:
-                    tool_calls = [f"Tool Name: {tc['name']} with Args: {tc['args']}" for tc in last_message.tool_calls]
-                    yield "All Tool Calls: " + "\n\n".join(tool_calls)
+                    if tool_name:
+                        tool_call_str = f"\n\n< TOOL CALL: {tool_name} >\n\n"
+                    if args:
+                        tool_call_str = args
 
-                yield last_message.content
+                    yield tool_call_str
+                else:
+                    yield message.content
                 continue
 
+        # if stream_mode == "updates" and isinstance(message_chunk, dict):
+        #     keys = list(message_chunk.keys())
+        #     graph_name = keys[0]
+        #     chunk = message_chunk[graph_name]
 
-async def main() -> None:
+        #     if isinstance(chunk, tuple) and isinstance(chunk[0], Interrupt):
+        #         interrupt_message = chunk[0].value
+        #         message = interrupt_message["message"]
+        #         tools_calls = interrupt_message["tool_call"]["name"]
+        #         yield f"\n\n {message} => **{tools_calls}**"
+        #         # yield "\n\n"
+
+        #     if isinstance(chunk, dict) and "messages" in chunk:  # messages
+        #         last_message = chunk["messages"][-1]
+
+        #         if isinstance(last_message, AIMessage) and last_message.tool_calls:
+        #             tool_calls = [f"Tool Name: {tc['name']} with Args: {tc['args']}" for tc in last_message.tool_calls]
+        #             yield "All Tool Calls: " + "\n\n".join(tool_calls)
+
+        #         yield last_message.content
+
+
+async def main() -> None:  # noqa: C901
     config = {
         "handlers": [
             {"sink": sys.stdout, "level": "ERROR", "colorize": True},
@@ -79,57 +103,56 @@ async def main() -> None:
                 ),
             ],
         )
-        # while True:
-        print(" ---- 🧞‍♀️ Assistant ---- \n")
-        async for response in stream_graph_responses(
-            user_input=graph_input,
-            graph=graph,
-            config=configurable,
-        ):
-            print(response, end="", flush=True)
-            print("-------------")
-        thread_state = graph.get_state(config=configurable)
-
-        # while thread_state.interrupts:
-        # handle interrupts
-        user_action = None
-        feedback = None
-        for interrupt in thread_state.interrupts:
-            print("\n ----- ✅ / ❌ Human Approval Required ----- \n")
-            interrupt_message = interrupt.value["message"]
-            tools_calls = interrupt.value["tool_call"]["name"]
-            print(f"{interrupt_message} => **{tools_calls}**")
-
-            action = input("Action: Accept or Feedback: ").strip().lower()
-            while action not in ["accept", "feedback"]:
-                action = input("Action: Accept or Feedback").strip().lower()
-
-            if action == "accept":
-                user_action = action
-            elif action == "feedback":
-                feedback = action
+        while True:
             print(" ---- 🧞‍♀️ Assistant ---- \n")
-            user_response = {"action": user_action, "feedback": feedback}
             async for response in stream_graph_responses(
-                user_input=Command(resume=user_response),
+                user_input=graph_input,
                 graph=graph,
                 config=configurable,
             ):
                 print(response, end="", flush=True)
             thread_state = graph.get_state(config=configurable)
 
-            # user_input = input("\n\nUser: ")
-            # if user_input.lower() in ["exit", "quit"]:
-            #     print("\n\nExit command received. Exiting...\n\n")
-            #     break
+            while thread_state.interrupts:  # handle interrupts
+                user_action = None
+                feedback = None
+                for interrupt in thread_state.interrupts:
+                    print("\n ----- ✅ / ❌ Human Approval Required ----- \n")
+                    interrupt_message = interrupt.value["message"]
+                    tools_calls = interrupt.value["tool_call"]["name"]
+                    print(f"{interrupt_message} => **{tools_calls}**")
 
-            # graph_input = {
-            #     "messages": [
-            #         HumanMessage(content=user_input),
-            #     ],
-            # }
+                    user_input = input("Action: Accept or Feedback: ").strip().lower()
+                    while user_input not in ["accept", "feedback"]:
+                        user_input = input("Action: Accept or Feedback: ").strip().lower()
 
-            # print(f"\n\n ----- 🥷 Human ----- \n\n{user_input}\n")
+                    # user can either accept or provide feedback
+                    if user_input == "accept":
+                        user_action = user_input
+                    elif user_input == "feedback":
+                        feedback = user_input
+                    print(" ---- 🧞‍♀️ Assistant ---- \n")
+                    user_response = {"action": user_action, "feedback": feedback}
+                    async for response in stream_graph_responses(
+                        user_input=Command(resume=user_response),
+                        graph=graph,
+                        config=configurable,
+                    ):
+                        print(response, end="", flush=True)
+                    thread_state = graph.get_state(config=configurable)
+
+            user_input = input("\n\nUser: ")
+            if user_input.lower() in ["exit", "quit"]:
+                print("\n\nExit command received. Exiting...\n\n")
+                break
+
+            graph_input = {
+                "messages": [
+                    HumanMessage(content=user_input),
+                ],
+            }
+
+            print(f"\n\n ----- 🥷 Human ----- \n\n{user_input}\n")
 
     except Exception as e:
         print(e)
